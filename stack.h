fa46ec8caea62dc2df7fba09863776e05b8fe3fc
HamzaYehia <hamzayehiamakhlouf@gmail.com>
2022-07-02 15:27:07 +0200

    updated stack

--- stack.h
+++ stack.h
@@ -6,36 +6,34 @@
 
 
 int stack[1024];
-int lastMember = 0;
+int lastMember = -1;
 
 void push(int member) {
     // ckeck if the stack is full
-    if (lastMember == sizeof(stack) / 4) {
-        printf("The stack is full, nothing pushed\n");
+    if (lastMember == (sizeof(stack) / 4) - 1) {
+        printf("The stack is full, %d was not pushed\n", member);
         return;
     }
+
     // else: push element on top of the Stack
-    stack[lastMember] = member;
+    stack[++lastMember] = member;
     printf("%d pushed at index %d\n", member, lastMember);
-    lastMember++;
 }
 
 int pop() {
     // ckeck if Stack is empty
-    lastMember--;
     if (lastMember < 0) {
         printf("Stack is empty, defualt value: ");
         return 0;
     }
     // else: return element on top of the Stack and remove it
     else {
-        return stack[lastMember];
+        return stack[lastMember--];
     }
 }
 
 void emptyStack() {
-    // empty the Stack
-    lastMember = 0;
+    lastMember = -1;
 }
 
 
